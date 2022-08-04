4f3bed7773d02ffbd8465076be8aa08b6bd358c0
gil01karougbe <essognimgilleskarougbe01@gmail.com>
2022-08-04 11:07:47 -0400

    some changes

---
+++ README.md
@@ -0,0 +1,15 @@
+#web scaping
+
+#content of the request
+this program is develloped to extract data on  https://wuzzuf.net/search/jobs/?q=python&a=hpb  web page
+it's written and presented in class (second semester 2022) by Ilham BENABID ,Ayoub LEHRAR and gilles KAROUGBE  as projet at the end of the python module.
+The responsible of the module is Pr Omar SEFRAOUI 
+
+
+
+ENSAO ,april 2022
+
+RUN
+1) pip install -r requiments.txt
+
+2) python webscaping.py
--- webscraping.py
+++ webscraping.py
@@ -5,7 +5,7 @@ from itertools import zip_longest
 
 
 
-
+#coluns
 job_title=[]
 company_name=[]
 location=[]
@@ -15,13 +15,23 @@ career_level=[]
 year_of_exper=[]
 skills=[]
 
+#request
 result=requests.get("https://wuzzuf.net/search/jobs/?q=python&a=hpb")
+#content of the request
 src=result.content
+
+#parsing in lxml
 soup=BeautifulSoup(src,"lxml")
+
+#geting all job titles
 job_titles=soup.find_all("h2",{"class":"css-m604qf"})
+#geting all companies name
 company_names=soup.find_all("a",{"class":"css-17s97q8"})
+#locations
 locations_names=soup.find_all("span",{"class":"css-5wys0k"})
+#date of publishment
 date=soup.find_all("div",{"class":"css-4c4ojb"})
+#job type
 jobtype=soup.find_all("span",{"class":"css-1ve4b75 eoyjyou0"})
 level=soup.find_all("a",{"class":"css--o171kl" , "href":"/a/Experienced-Jobs-in-Egypt"})
 exp_year=soup.find_all("span")
@@ -31,11 +41,13 @@ job_skills=soup.find_all("div",{"class":"css-y4udm8"})
 
 
 
+#reading informations
 for i in range (len(job_titles)):
     job_title.append(job_titles[i].text)
     company_name.append(company_names[i].text)
     location.append(locations_names[i].text)
     job_type.append(jobtype[i].text)
+    #skills needed for a well known job
     for j in range (len(job_skills)):
         skills.append(job_skills[i].text)
 
@@ -43,17 +55,10 @@ for i in range (len(job_titles)):
 
 
 
-
+#writting in a csv file
 file_list = [job_title, company_name, location, job_type, skills]
 exported = zip_longest(*file_list)
 with open(r"C:\Users\hhss\Documents\JOBSTEXT.csv" , "w") as myfile:
     wr = csv.writer(myfile)
     wr.writerow(["job_title", "company_name", "location", "job_type", "skills"])
     wr.writerows(exported)
-
-
-#for i in range(len(job_titles)):
-    #job_type.append(jobtype[i].text)
-
-
-#print(job_type)
